11ffb42216ade08ea19f35a7b6abcc149c979203
Xavier <xavier.s.viel@gmail.com>
2024-11-29 14:14:20 -0400

    commentaires

--- projet.py
+++ projet.py
@@ -4,6 +4,7 @@ dsn_name = "projetmysql"
 user_id = "elf"
 password = "admin"
 
+#cet utilisateur a seulement acces a la database christmasshop en select, il ne peut pas faire d'autres actions
 try:
     cnxn = pyodbc.connect(f"DSN={dsn_name};UID={user_id};PWD={password};")
     print("connexion reussie via DSN avec UID et PWD")
