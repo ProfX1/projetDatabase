4f31bf325d57e4659801c374f748b475323fa765
Xavier <xavier.s.viel@gmail.com>
2024-11-29 13:42:11 -0400

    etape2

---
+++ etape2.py
@@ -0,0 +1,19 @@
+import pyodbc
+
+dsn_name = "projetDatabase"
+user_id = "santa"
+password = "admin"
+
+try:
+    cnxn = pyodbc.connect(f"DSN={dsn_name};UID={user_id};PWD={password};")
+    print("connexion reussie via DSN avec UID et PWD")
+    
+    cursor = cnxn.cursor()
+    cursor.execute("select * from customers")
+    for row in cursor.fetchall():
+        print(row)
+        
+    cnxn.close()
+    
+except pyodbc.Error as ex:
+    print("erreur lors de la connexion:", ex)
