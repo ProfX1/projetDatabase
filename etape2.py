54370bb67d4135e71ba083331519cfc254a2e344
Xavier <xavier.s.viel@gmail.com>
2024-11-29 14:22:08 -0400

    description of sale

--- etape2.py
+++ etape2.py
@@ -9,7 +9,7 @@ try:
     print("connexion reussie via DSN avec UID et PWD")
     
     cursor = cnxn.cursor()
-    
+    #John doe wants to buy 5 christmas trees, a sales gets inserted in sales with the customer_id and the product_id, when a producct gets sold the quantity sold gets removed from the quantity of the product sold
     cursor.execute("SET autocommit = 0")
     cursor.execute("BEGIN WORK")
     cursor.execute("update products set quantity = 15 where product_id=1")
