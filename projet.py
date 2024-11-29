d62cb94ecf4d5564ac273ed8eb3232b850ca57a1
Xavier <xavier.s.viel@gmail.com>
2024-11-29 13:15:00 -0400

    test

--- projet.py
+++ projet.py
@@ -1,15 +1,15 @@
 import pyodbc
 
-dsn_name = "3dhub"
-user_id = "python"
-password = "python"
+dsn_name = "christmasshop"
+user_id = "elf"
+password = "admin"
 
 try:
     cnxn = pyodbc.connect(f"DSN={dsn_name};UID={user_id};PWD={password};")
     print("connexion reussie via DSN avec UID et PWD")
     
     cursor = cnxn.cursor()
-    cursor.execute("select * from costs where listed_price >= 110")
+    cursor.execute("select * from customers")
     for row in cursor.fetchall():
         print(row)
         
