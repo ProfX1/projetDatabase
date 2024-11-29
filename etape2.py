78c7bdba14432ff160a392bc44edbc10ec371697
Xavier <xavier.s.viel@gmail.com>
2024-11-29 14:28:59 -0400

    order

--- etape2.py
+++ etape2.py
@@ -12,6 +12,13 @@ try:
     #John doe wants to buy 5 christmas trees, a sales gets inserted in sales with the customer_id and the product_id, when a producct gets sold the quantity sold gets removed from the quantity of the product sold
     cursor.execute("SET autocommit = 0")
     cursor.execute("BEGIN WORK")
+    print('who is ordering something')
+    name=input()
+    print ('what is he ordering')
+    item=input()
+    print('how many of this item')
+    quantity=int(input())
+    order=(name, item, quantity)
     cursor.execute("update products set quantity = 15 where product_id=1")
     cursor.execute("select quantity from products")
     
