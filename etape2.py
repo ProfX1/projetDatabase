e26d599a97bd7a244093de8df36114ce3aa3e258
Xavier <xavier.s.viel@gmail.com>
2024-11-30 13:43:06 -0400

    check if customer exists in database if not then add customer to database

--- etape2.py
+++ etape2.py
@@ -19,16 +19,71 @@ try:
     print('how many of this item')
     quantity=int(input())
     order=(name, item, quantity)
-    cursor.execute("update products set quantity = 15 where product_id=1")
-    cursor.execute("select quantity from products")
+    #select last inserted id
+    select_id = "SELECT LAST_INSERT_ID()"
+    
+    #confirmation stuff customer
+    select_customer_query = "select customer_id, customer_name from customers where customer_name = ?"
+    cursor.execute(select_customer_query, name)
+    customer_query = cursor.fetchall()
+    #select customer id
+    select_customer_id = "select customer_id from customers where customer_name = ?"
+    cursor.execute(select_customer_id, name)
+    customer_id_tuple = cursor.fetchone()
+    if customer_id_tuple is None:
+        print('please enter a phone number for the customer')
+        phone_number = input()
+        insert_customer = "insert into customers (customer_name, customer_phone_number) values(?, ?)"
+        cursor.execute(insert_customer, name, phone_number)
+        cursor.execute(select_id)
+        customer_id_tuple = cursor.fetchone()
+        
+    customer_id = customer_id_tuple[0]
+    print (customer_id)
+    #confirmation stuff product
+    select_item_query = "select product_id, product_name from products where product_name = ?"
+    cursor.execute(select_item_query, item)
+    select_query = cursor.fetchall()
+    #select product_id
+    select_item_id = "select product_id from products where product_name = ?"
+    cursor.execute(select_item_id, item)
+    item_id_tuple = cursor.fetchone()
+    item_id = item_id_tuple[0]
+    print (item_id)
+    #confirmation stuff product again
+    select_quantity_query = "select product_name, quantity from products where product_name = ?"
+    cursor.execute(select_quantity_query, item)
+    quantity_query = cursor.fetchall()
+    for row in customer_query:
+        print(row)
+    for row in select_query:
+        print(row)
+    for row in quantity_query:
+        print(row)
+        
+    # now to add the sale in the database
+    
+    
+    # cursor.execute("update products set quantity = 15 where product_id=1")
+    # cursor.execute("select quantity from products")
     
 
-    for row in cursor.fetchall():
-        print(row)
+    # for row in cursor.fetchall():
+    #     print(row)
     print('est-ce que tout est bon yes or no')
     answer = input()
-    if answer =='yes':    
-        cnxn.execute('COMMIT')
+    if answer =='yes':
+        # now to add the sale in the database
+        insert_sales = "insert into sales (customer_id, product_id) values (?, ?)"
+        insert_order_products = "insert into order_products (product_id) values (?)"
+        remove_quantity = "update"
+        print('est-ce que tout est bon yes or no')
+        answer1 = input()
+        if answer1 == 'yes':
+                
+            cnxn.execute('COMMIT')
+        else:
+            cnxn.execute('ROLLBACK')
     else :
         cnxn.execute('rollback')
     cnxn.close()
